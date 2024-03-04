5559f766d33539065e0633d56699c861afa6db68
Evan <evanjian@buffalo.edu>
2024-03-04 16:25:16 -0500

    added more routes to flask setup

--- server.py
+++ server.py
@@ -4,3 +4,11 @@ app = Flask(__name__)
 
 #@app.route("/")
 #TODO: Landing Page
+
+#@app.route("/register")
+#TODO: Register
+
+#@app.route("/login")
+#TODO: Login
+
+#Add in the CSS, JS, and Image Folder
