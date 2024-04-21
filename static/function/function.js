b97f8a9b90b40a65cf836dde5e11d12804ea66f3
kanghaol <kanghaol@buffalo.edu>
2024-04-21 16:52:08 -0400

    socket change

--- static/function/function.js
+++ static/function/function.js
@@ -1,4 +1,4 @@
-const socket = io.connect('http://localhost:8080'); 
+const socket = io.connect('https://tbd312.me'); 
 
 function submitPost() {
     const messageInput = document.getElementById('message');
